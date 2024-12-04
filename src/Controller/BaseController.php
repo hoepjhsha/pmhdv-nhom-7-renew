503f1e953464fb56db17b1d04e56a73f3ed79f19
hoep <hiepnguyen3624@gmail.com>
2024-12-04 11:21:59 +0700

    Init

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @project pmhdv-nhom-7-renew
+ * @author hoepjhsha
+ * @email hiepnguyen3624@gmail.com
+ * @date 04/12/2024
+ * @time 11:12
+ */
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+/**
+ * Class BaseController
+ *
+ * Base controller for all controllers in the application
+ *
+ * Extend this class in new controllers to inherit common methods
+ * ```
+ *  class NewController extends BaseController
+ * ```
+ *
+ * For security be sure to declare all methods as protected or private
+ */
+class BaseController extends AbstractController
+{
+
+}
